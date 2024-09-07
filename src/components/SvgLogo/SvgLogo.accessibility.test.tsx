1ccfe5b1c81a97384468e8e6c198be79e3a051ed
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-09-07 19:14:09 -0300

    Improving the accessibility test

--- src/components/SvgLogo/SvgLogo.accessibility.test.tsx
+++ src/components/SvgLogo/SvgLogo.accessibility.test.tsx
@@ -6,12 +6,14 @@ import { describe, expect, test } from "vitest";
 import SvgLogo from ".";
 
 describe("Testing if the accessibility of the svg logo", () => {
-	test("has the correct role and alt text", () => {
+	test("has the correct role and alt text with aria-label", () => {
 		render(<SvgLogo />);
 
 		const svgLogo = screen.getByTestId("SvgLogo");
 
 		expect(svgLogo.getAttribute("role")).toBe("img");
-		expect(svgLogo.getAttribute("aria-label")).toBeDefined();
+		expect(svgLogo.getAttribute("aria-label")).toBe(
+			"The page logo: a light blue circle filled with a couple of wavy horizontal lines with a solid smaller circle in the center"
+		);
 	});
 });

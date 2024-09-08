2ec98e8cf832c11f015f740fbbb8e1075fd57bc3
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-09-07 21:06:44 -0300

    Adding the new component to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import styles from "./page.module.scss";
 // Custom components
 import SvgLogo from "@/components/SvgLogo";
+import HeadArticle from "@/components/HeadArticle";
 
 export default function HomePage() {
 	return (
 		<section className={styles.homePage}>
 			<SvgLogo />
+			<HeadArticle />
 		</section>
 	);
 }

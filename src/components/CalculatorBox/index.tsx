c2214181f8291be4488945279e77b9ecac4dea30
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-09-09 17:41:13 -0300

    Starting the CalculatorBox component

--- src/components/CalculatorBox/index.tsx
+++ src/components/CalculatorBox/index.tsx
@@ -3,14 +3,30 @@ import React from "react";
 import styles from "./CalculatorBox.module.scss";
 // Custom components
 import Wrapper from "../Wrapper";
+import InputRadio from "./InputRadio";
 
 const CalculatorBox: React.FC = () => {
 	return (
 		<Wrapper data-testid="calculatorWrapper" tagKind="section">
 			<section data-testid="calculatorBox" className={styles.calculatorBox}>
-				CalculatorBox
+				<h2 className={styles.calculatorBox__heading}>
+					Enter your details below
+				</h2>
+				<div className={styles.calculatorBox__unitsDiv}>
+					<div className={styles.calculatorBox__unitsDiv__unit}>
+						<InputRadio />
+						<span className={styles.calculatorBox__unitsDiv__unit__type}>
+							Metric
+						</span>
+					</div>
+					<div className={styles.calculatorBox__unitsDiv__unit}>
+						<InputRadio />
+						<span className={styles.calculatorBox__unitsDiv__unit__type}>
+							Imperial
+						</span>
+					</div>
+				</div>
 			</section>
-			;
 		</Wrapper>
 	);
 };
